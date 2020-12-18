02c5216a87a6f22d54e4dd90b8b717f3f3d9ef80
Andrea Serboli <andrea.serboli12@gmail.com>
2020-12-18 16:31:23 +0100

    miglioramenti css

--- js/boolflix.js
+++ js/boolflix.js
@@ -1,6 +1,6 @@
 var app = new Vue(
   {
-    el: '#dischi',
+    el: '#flix',
     data: {
       libreriaFilm: [],
       ricerca: ''
@@ -10,22 +10,25 @@ var app = new Vue(
     },
     methods: {
       cercaFilm: function() {
-          axios.get('https://api.themoviedb.org/3/search/movie?', {
-            params: {
-              api_key: 'd8981d977157659ad91b21c7715d6d58',
-              query: this.ricerca,
-              language: 'it-IT'
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+              params: {
+                api_key: 'd8981d977157659ad91b21c7715d6d58',
+                query: this.ricerca,
+                language: 'it-IT'
+              }
             }
+          )
+          .then( (result) => {
+            this.libreriaFilm = result.data.results;
+            console.log(result)
+            console.log(this.libreriaFilm)
           }
+
         )
-        .then( (result) => {
-          this.libreriaFilm = result.data.response
-        }
 
-      )
-        console.log(this.libreriaFilm)
       }
-
+    },
+    mounted: function() {
 
     }
   }
