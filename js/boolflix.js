471f2f06325f0bdd1261c36bf760ec9dbe2350e6
Andrea Serboli <andrea.serboli12@gmail.com>
2020-12-22 14:15:18 +0100

    milestone 5

--- js/boolflix.js
+++ js/boolflix.js
@@ -5,6 +5,8 @@ var app = new Vue(
       libreriaFilm: [],
       libreriaSerieTV : [],
       libreriaCompleta: [],
+      generi: [] ,
+      genereSelezionato: 'All',
       ricerca: '' ,
     },
     created: function() {
@@ -12,6 +14,7 @@ var app = new Vue(
     },
     methods: {
       cercaFilm: function() {
+
         if(this.ricerca != '') {
             axios.get('https://api.themoviedb.org/3/search/movie', {
                 params: {
@@ -27,40 +30,53 @@ var app = new Vue(
               for(var i = 0; i < this.libreriaFilm.length; i++) {
                 this.libreriaFilm[i].vote_average = Math.ceil(this.libreriaFilm[i].vote_average/2)
               }
-            }
-          )
-          axios.get('https://api.themoviedb.org/3/search/tv' , {
-              params: {
-                api_key: 'e99307154c6dfb0b4750f6603256716d',
-                query: this.ricerca,
-                language: 'it-IT'
-              }
-            }
-          )
-          .then( (series) => {
-              this.libreriaSerieTV = series.data.results;
-              console.log(this.libreriaSerieTV)
-              for(var i = 0; i < this.libreriaSerieTV.length; i++) {
-                this.libreriaSerieTV[i].vote_average = Math.ceil(this.libreriaSerieTV[i].vote_average/2)
-              }
-              this.libreriaCompleta = [...this.libreriaFilm, ...this.libreriaSerieTV]
-              this.libreriaCompleta.sort(function(a, b) {
-                  if(b.popularity < a.popularity){
-                    return -1;
+
+              // 2° chiamata serie tv
+              axios.get('https://api.themoviedb.org/3/search/tv' , {
+                  params: {
+                    api_key: 'e99307154c6dfb0b4750f6603256716d',
+                    query: this.ricerca,
+                    language: 'it-IT'
                   }
-                  if(b.popularity > a.popularity) {
-                    return 1;
-                  }
-                  return 0;
                 }
               )
-              this.libreriaCompleta.sort()
-              console.log(this.libreriaCompleta)
+              .then( (series) => {
+                  this.libreriaSerieTV = series.data.results;
+                  console.log(this.libreriaSerieTV)
+                  for(var i = 0; i < this.libreriaSerieTV.length; i++) {
+                    this.libreriaSerieTV[i].vote_average = Math.ceil(this.libreriaSerieTV[i].vote_average/2)
+                  }
+                  this.libreriaCompleta = [...this.libreriaFilm, ...this.libreriaSerieTV]
+                  this.libreriaCompleta.sort(function(a, b) {
+                      if(b.popularity < a.popularity){
+                        return -1;
+                      }
+                      if(b.popularity > a.popularity) {
+                        return 1;
+                      }
+                      return 0;
+                    }
+                  )
+                  this.libreriaCompleta.sort()
+                  console.log(this.libreriaCompleta)
+
+                  axios.get('https://api.themoviedb.org/3/genre/movie/list', {
+                    params: {
+                      api_key: 'd8981d977157659ad91b21c7715d6d58',
+                      query: this.ricerca,
+                      language: 'it-IT',
+                    }
+                  })
+                  .then((generi) => {
+                    this.generi = generi.data.genres;
+                    console.log(this.generi)
+                  })
+                }
+              )
+
             }
-          )
+          );
         }
-
-
       }
     },
     mounted: function() {
